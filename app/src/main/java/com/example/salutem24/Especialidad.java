a84645636b127d6d5cc1ac9c8d21ae698d4b173b
Cristopher Torres <cristopher.tmar@gmail.com>
2020-08-04 18:50:04 -0500

    Se agregó pantalla de reserva de consultas

--- app/src/main/java/com/example/salutem24/Especialidad.java
+++ app/src/main/java/com/example/salutem24/Especialidad.java
@@ -1,14 +1,37 @@
 package com.example.salutem24;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Especialidad extends AppCompatActivity {
+
+    LinearLayout ly_general;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_especialidad );
+
+
+        ly_general = findViewById( R.id.ly_general );
+
+        ly_general.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( Especialidad.this, Horarios.class);
+                startActivity( intent );
+                finish();
+            }
+        } );
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/salutem24/Horarios.java
@@ -0,0 +1,82 @@
+package com.example.salutem24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+
+import java.util.Calendar;
+
+public class Horarios extends AppCompatActivity {
+
+    Spinner sp_medico;
+    EditText et_fecha;
+    Button btn_editarFiltros, btn_buscar;
+    LinearLayout ly_filtros;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_horarios );
+
+        sp_medico = findViewById( R.id.sp_medico );
+        et_fecha = findViewById( R.id.et_fecha );
+        btn_editarFiltros = findViewById( R.id.btn_editarFiltros );
+        ly_filtros = findViewById( R.id.ly_filtros );
+        btn_buscar = findViewById( R.id.btn_buscar );
+
+        et_fecha.setFocusable(false);
+        et_fecha.setKeyListener(null);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource( this, R.array.Medicos, android.R.layout.simple_spinner_item );
+        adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+        sp_medico.setAdapter( adapter );
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+        et_fecha.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog( Horarios.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        month = month + 1;
+                        String date = day + "/" + month + "/" + year;
+                        et_fecha.setText( date );
+                    }
+                }, year, month, day );
+                datePickerDialog.show();
+            }
+        } );
+
+        btn_editarFiltros.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ly_filtros.setVisibility( View.VISIBLE );
+                btn_editarFiltros.setVisibility( View.GONE );
+            }
+        } );
+
+        btn_buscar.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ly_filtros.setVisibility( View.GONE );
+                btn_editarFiltros.setVisibility( View.VISIBLE );
+            }
+        } );
+
+    }
+}
